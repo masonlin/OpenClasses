8a93ab905e30a0c8d051c1df81a1760778527703
masonlin <a1807d@yahoo.com.tw>
2015-08-20 05:44:03 +0800

    Human Classes
    About the human classes will join this namespace.

---
+++ Human/src/Human/Human.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Human
+{
+    // This project can output the Class library as a NuGet Package.
+    // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
+
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Person
+    {
+        public Person()
+        {
+
+
+
+        }
+
+    }
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Brain
+    {
+        public Brain()
+        {
+
+        }
+    
+    }
+
+
+
+
+
+
+}
